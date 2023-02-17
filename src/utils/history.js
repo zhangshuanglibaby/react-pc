94c1258586b432d30b6ed54d234f2d987e22f98d
zhangshuangli <943165920@qq.com>
2023-02-17 11:24:32 +0800

    update

--- src/utils/history.js
+++ src/utils/history.js
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2023-02-17 10:41:01
+ * @LastEditors: zhangshuangli
+ * @LastEditTime: 2023-02-17 11:24:11
+ * @Description: 这是****文件
+ */
 import { createBrowserHistory } from 'history'
 
 const history = createBrowserHistory()
